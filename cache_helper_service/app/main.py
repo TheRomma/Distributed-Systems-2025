27b9b40026e4e206d3fb9d455b5ec155aad29463
Jere Koivisto <jere.koivisto@vtt.fi>
2025-02-25 11:07:47 +0200

    Added cache-helper-service. General cleanup.

---
+++ cache_helper_service/app/main.py
@@ -0,0 +1,50 @@
+from openai import OpenAI
+import json
+import os
+from fastapi import FastAPI
+import requests
+
+client = OpenAI(
+	api_key = os.getenv("API_KEY", "my_key")
+)
+
+examples = '''
+		"input" : "Could you cache the following video [given_link] with the following title [given_title]?"
+		"output" : {req=[{"link" : "given_link", "title" : "given_title"}]}
+		"input" : "Cache [given_link] with the title [given_title]."
+		"output" : {req=[{"link" : "given_link", "title" : "given_title"}]}
+		"input" : "Could you remove [given_filename] from the cache?"
+		"output" : {req=[{"filename" : "given_filename"}]}
+		"input" : "Remove [given_filename]."
+		"output" : {req=[{"filename" : "given_filename"}]}
+		"input" : "Remove [given_filename] and [another_filename]."
+		"output" : {req=[{"filename" : "given_filename"}, {"filename" : "another_filename"}]}
+'''
+
+app = FastAPI()
+
+@app.post("/prompt")
+async def process_message(req: str):
+	prompt = "Translate the following request into a dictionary and do not include the input: " + examples + "\nRequest: " + req
+
+	response = client.chat.completions.create(
+		model="gpt-3.5-turbo",
+		messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ],
+		max_tokens=50
+	)
+
+	result = json.loads(response.choices[0].message.content)
+	for thing in result['req']:
+		if "link" in thing and "title" in thing:
+			print(thing)
+			resp = requests.post("http://content-distribution-service-service:8123/add", params=thing)
+		elif "filename" in thing:
+			print(thing)
+			resp = requests.post("http://content-distribution-service-service:8123/remove", params=thing)
+		else:
+			print("Could not interpret instructions!")
+			
+	return {"status": response.choices[0].message.content}
--- edge/mongodb/readme.txt
+++
@@ -1,5 +0,0 @@
-Pull image:
-docker pull mongodb/mongodb-community-server:latest
-
-Run container:
-docker run --name mongodb -p 27017:27017 -d mongodb/mongodb-community-server:latest
